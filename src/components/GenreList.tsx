ba4c5bb944ec627528f86d39b08e2efc42951b60
Homesh Y <homesh.yellenti@deltax.com>
2023-06-11 20:24:30 +0530

    create a generic data fetching hook

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import useGenres, { Genre } from '../hooks/useGenres';
+
+
+
+const GenreList = () => {
+
+    const {data} = useGenres()
+
+  return (
+    <ul>
+        {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
+    </ul>
+  )
+}
+
+export default GenreList

4cdf6cd8aef29bf5c218cbef83cba2e715b9a7cd
Homesh Y <homesh.yellenti@deltax.com>
2023-06-11 20:50:33 +0530

    display sidebar styles fixed

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,11 +12,20 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{sm : 1, md: 2, lg: 3, xl : 4}} padding='10px' spacing={10}>
-        {isLoading && skeletons.map(skeleton => <GameCardContainer><GameCardSkeleton key={skeleton}/></GameCardContainer> )}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding="10px"
+        spacing={3}
+      >
+        {isLoading &&
+          skeletons.map((skeleton, index) => (
+            <GameCardContainer key={index}>
+              <GameCardSkeleton key={index} />
+            </GameCardContainer>
+          ))}
         {data.map((game) => (
-          <GameCardContainer>
-              <GameCard key={game.id} game={game}></GameCard>
+          <GameCardContainer key={game.id}>
+            <GameCard key={game.id} game={game}></GameCard>
           </GameCardContainer>
         ))}
       </SimpleGrid>
